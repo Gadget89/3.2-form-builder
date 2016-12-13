332bd4b1338004d6b8eb3698761436851b019187
Nicolas Roybal <nicolas.roybal@hotmail.com>
2016-12-13 16:52:58 -0600

    Made progress on getting data out by type

--- js/main.js
+++ js/main.js
@@ -6,9 +6,29 @@ document.addEventListener("DOMContentLoaded", function(){
     var xhrData = this.response;
     var formData = JSON.parse(xhrData);
 
+    var inputElement = document.querySelector("[data-js='body']");
+
     formData.forEach(function(arrayObject){
-      var arraySearch = arrayObject.label;
-    
+      //var arraySearch = arrayObject.label;
+      var finalForm = "";
+
+      if (arrayObject.type === "text"{
+        finalForm += `<text>`;
+      })
+
+      if (formData.type === "select"){
+        finalForm += `<select>`;
+        formData.options.forEach(function(optionData){
+          finalForm += `<option label="${optionData.label}"
+          value="${optionData.id}" icon="${options.icon}">`;
+        });
+        console.log();
+      }else {
+        return"it works";
+      }
+
+      var eachObject = `<input type="${arrayObject.type}" id="${arrayObject.id}">`;
+      console.log(eachObject);
     })
   })
   xhr.send();
