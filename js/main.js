af410732bf3b3eb66e60217e2eaef3c8d348c0bc
Nicolas Roybal <nicolas.roybal@hotmail.com>
2016-12-13 14:51:09 -0600

    Made progress parseing data and started on for each loop

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 // Main JS //
+document.addEventListener("DOMContentLoaded", function(){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://json-data.herokuapp.com/forms");
+  xhr.addEventListener("load", function(e){
+    var xhrData = this.response;
+    var formData = JSON.parse(xhrData);
+
+    formData.forEach(function(arrayObject){
+      var arraySearch = arrayObject.label;
+    
+    })
+  })
+  xhr.send();
+})
