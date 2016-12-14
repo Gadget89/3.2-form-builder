46a5026627e409c0f6a54e118ba73bcd1a0246fe
Nicolas Roybal <nicolas.roybal@hotmail.com>
2016-12-13 21:48:51 -0600

    debugged js to diplay input data correctly

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function(){
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "http://json-data.herokuapp.com/forms");
   xhr.addEventListener("load", function(e){
-    var xhrData = this.response;
-    var formData = JSON.parse(xhrData);
+    //var xhrData = this.response;
+    var formData = JSON.parse(this.response);
 
     var inputElement = document.querySelector("[data-js='form']");
 
@@ -13,13 +13,12 @@ document.addEventListener("DOMContentLoaded", function(){
     formData.forEach(function(arrayObject){
       var arrayObjectPin = "";
       if (arrayObject.type === "select"){
-        arrayObjectPin += `<select>`;
-        arrayObject.options.forEach(function(optionData){
-          arrayObjectPin += `<option label="${optionData.label}" value="${optionData.value}"</option>`;
+        arrayObjectPin += `<select class="selector">`;
+          arrayObject.options.forEach(function(optionData){
+            arrayObjectPin += `<option label="${optionData.label}" value="${optionData.value}"</option>`;
         })
     }else{
-      arrayObjectPin += `<label>${arrayObject.label}</label>`;
-      arrayObjectPin += `<input class="textInput" type="${arrayObject.type}" id="${arrayObject.id}" icon="${arrayObject.icon}">`;
+      arrayObjectPin += `<input class="textInput" placeholder="${arrayObject.label}" type="${arrayObject.type}" id="${arrayObject.id}" icon="${arrayObject.icon}">`;
     }
     allInputData += arrayObjectPin;
   });
